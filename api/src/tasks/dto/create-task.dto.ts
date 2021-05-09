8bfe11defcfe511fb3a2cf9327a45bae0ac526f5
tk1351 <takakikitsui0111@gmail.com>
2021-05-09 09:10:08 +0900

    createTaskへのvalidation追加

--- api/src/tasks/dto/create-task.dto.ts
+++ api/src/tasks/dto/create-task.dto.ts
@@ -1,5 +1,10 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class CreateTaskDto {
   // createTask()の引数に入る値の型を定義する
+  @IsNotEmpty()
   title: string;
+
+  @IsNotEmpty()
   description: string;
 }
