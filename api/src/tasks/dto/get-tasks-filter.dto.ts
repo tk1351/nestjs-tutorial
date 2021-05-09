f28398c0cbb56c454acdf9e54ee853da73bddb77
tk1351 <takakikitsui0111@gmail.com>
2021-05-09 18:57:20 +0900

    filterのvalidation追加、postgreSQLの環境構築

--- api/src/tasks/dto/get-tasks-filter.dto.ts
+++ api/src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,15 @@
 import { TaskStatus } from '../task.model';
+import { IsOptional, IsIn, IsNotEmpty } from 'class-validator';
 
 export class GetTasksFilterDto {
+  // statusもsearchも必須ではないため@IsOptional()を使う
+  // @IsInで配列内の値のいずれかと一致するか判定する
+  @IsOptional()
+  @IsIn([TaskStatus.OPEN, TaskStatus.IN_PROGRESS, TaskStatus.DONE])
   status: TaskStatus;
+
+  // serachのqueryが存在する場合は@IsNotEmpty()が発動する
+  @IsOptional()
+  @IsNotEmpty()
   search: string;
 }
