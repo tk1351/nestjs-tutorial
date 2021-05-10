4c9b60e30978ec13d85a0db5dd5d4066e0564a53
tk1351 <takakikitsui0111@gmail.com>
2021-05-11 08:14:48 +0900

    getTasksByIdの記述変更

--- api/src/tasks/tasks.service.ts
+++ api/src/tasks/tasks.service.ts
@@ -17,8 +17,10 @@ export class TasksService {
   getTasks(filterDto: GetTasksFilterDto, user: User): Promise<Task[]> {
     return this.taskRepository.getTasks(filterDto, user);
   }
-  async getTaskById(id: number): Promise<Task> {
-    const found = await this.taskRepository.findOne(id);
+  async getTaskById(id: number, user: User): Promise<Task> {
+    const found = await this.taskRepository.findOne({
+      where: { id, userId: user.id },
+    });
 
     if (!found) {
       throw new NotFoundException(`ID: ${id}のtaskは存在しません`);
@@ -39,10 +41,10 @@ export class TasksService {
     }
   }
 
-  async updateTaskStatus(id: number, status: TaskStatus): Promise<Task> {
-    const task = await this.getTaskById(id);
-    task.status = status;
-    await task.save();
-    return task;
-  }
+  // async updateTaskStatus(id: number, status: TaskStatus): Promise<Task> {
+  //   const task = await this.getTaskById(id);
+  //   task.status = status;
+  //   await task.save();
+  //   return task;
+  // }
 }
