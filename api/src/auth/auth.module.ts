188787a9540c82a08b4beafe3af1a4f10f119faf
tk1351 <takakikitsui0111@gmail.com>
2021-05-10 20:57:04 +0900

    jwtとpassportのインストール

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -3,9 +3,22 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWTSECRET,
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+    }),
+    TypeOrmModule.forFeature([UserRepository]),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
