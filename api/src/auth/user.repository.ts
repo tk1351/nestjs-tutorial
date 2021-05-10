c76bf3175115b8e77553c81f9b536d1bdbee845a
tk1351 <takakikitsui0111@gmail.com>
2021-05-10 14:20:16 +0900

    username重複のエラーハンドリング

--- api/src/auth/user.repository.ts
+++ api/src/auth/user.repository.ts
@@ -1,6 +1,10 @@
 import { Repository, EntityRepository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
@@ -11,6 +15,15 @@ export class UserRepository extends Repository<User> {
     user.username = username;
     user.password = password;
 
-    await user.save();
+    try {
+      await user.save();
+    } catch (err) {
+      // usernameの重複
+      if (err.code === '23505') {
+        throw new ConflictException('Usernameは既に存在します');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
