894ffceaed77470a0ab40744cc2f601f41207619
tk1351 <takakikitsui0111@gmail.com>
2021-05-10 14:08:48 +0900

    auth dtoへのvalidation追加

--- api/src/auth/dto/auth-credentials.dto.ts
+++ api/src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,17 @@
+import { IsString, MinLength, MaxLength, Matches } from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
+  // 大文字が必要などを定義できる
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'パスワードの強度が弱いです',
+  })
   password: string;
 }
