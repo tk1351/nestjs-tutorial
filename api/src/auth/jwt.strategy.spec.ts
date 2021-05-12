75bee16c7afd4942c3952a8d3ab7b2b46ad2272c
tk1351 <takakikitsui0111@gmail.com>
2021-05-12 12:48:57 +0900

    jwt.strategyのテスト追加

---
+++ api/src/auth/jwt.strategy.spec.ts
@@ -0,0 +1,47 @@
+import { JwtStrategy } from './jwt.strategy';
+import { Test } from '@nestjs/testing';
+import { UserRepository } from './user.repository';
+import { User } from './user.entity';
+import { UnauthorizedException } from '@nestjs/common';
+
+const mockUserRepository = () => ({
+  findOne: jest.fn(),
+});
+
+describe('JwtStrategy', () => {
+  let jwtStrategy: JwtStrategy;
+  let userRepository;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        JwtStrategy,
+        { provide: UserRepository, useFactory: mockUserRepository },
+      ],
+    }).compile();
+
+    jwtStrategy = await module.get<JwtStrategy>(JwtStrategy);
+    userRepository = await module.get<UserRepository>(UserRepository);
+  });
+
+  describe('validate', () => {
+    it('JWT payloadsに基づいてバリデーションをし、userを返す', async () => {
+      const user = new User();
+      user.username = 'TestUser';
+
+      userRepository.findOne.mockResolvedValue(user);
+      const result = await jwtStrategy.validate({ username: 'TestUser' });
+      expect(userRepository.findOne).toHaveBeenCalledWith({
+        username: 'TestUser',
+      });
+      expect(result).toEqual(user);
+    });
+
+    it('userが見つからない場合、errorを返す', async () => {
+      userRepository.findOne.mockResolvedValue(null);
+      expect(jwtStrategy.validate({ username: 'TestUser' })).rejects.toThrow(
+        UnauthorizedException,
+      );
+    });
+  });
+});
